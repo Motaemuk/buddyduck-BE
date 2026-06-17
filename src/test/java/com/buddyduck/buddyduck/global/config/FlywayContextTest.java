49d3b8aaa67c24f3c420abe557adbe35fcf710e0
chazy-d <my0710my@gmail.com>
2026-06-17 16:07:10 +0900

    fix: OAuth rebase 후 테스트 충돌 정리

--- src/test/java/com/buddyduck/buddyduck/global/config/FlywayContextTest.java
+++ src/test/java/com/buddyduck/buddyduck/global/config/FlywayContextTest.java
@@ -17,9 +17,12 @@ class FlywayContextTest {
 	@Test
 	void Flyway가_schema_migration을_관리한다() {
 		MigrationInfo currentMigration = flyway.info().current();
+		MigrationInfo[] appliedMigrations = flyway.info().applied();
 
 		assertThat(currentMigration).isNotNull();
-		assertThat(currentMigration.getVersion().getVersion()).isEqualTo("1");
 		assertThat(currentMigration.getState().isApplied()).isTrue();
+		assertThat(appliedMigrations)
+			.extracting(migration -> migration.getVersion().getVersion())
+			.contains("1");
 	}
 }

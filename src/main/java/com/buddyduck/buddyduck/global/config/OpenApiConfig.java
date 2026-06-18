b22158220bd85faa6fc898734431f4041377b7fb
chazy-d <my0710my@gmail.com>
2026-06-19 00:08:36 +0900

    docs: Swagger JWT 인증 설정 추가

--- src/main/java/com/buddyduck/buddyduck/global/config/OpenApiConfig.java
+++ src/main/java/com/buddyduck/buddyduck/global/config/OpenApiConfig.java
@@ -1,16 +1,33 @@
 package com.buddyduck.buddyduck.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+	private static final String BEARER_AUTH = "bearerAuth";
+
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
+			.servers(List.of(
+				new Server().url("https://api.boostad.site").description("Production API"),
+				new Server().url("http://localhost:8080").description("Local API")
+			))
+			.components(new Components()
+				.addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")))
+			.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
 			.info(new Info()
 				.title("Buddyduck API")
 				.description("Concert Buddy backend API")
